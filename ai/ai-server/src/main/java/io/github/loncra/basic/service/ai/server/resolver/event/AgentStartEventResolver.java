a1aac2d86cfda7ab84f254e6fa8633056ef85565
chenxiaobo <chan.alex@foxmail.com>
2026-07-30 12:24:10 +0800

    还原手多删除的代码。

---
+++ ai/ai-server/src/main/java/io/github/loncra/basic/service/ai/server/resolver/event/AgentStartEventResolver.java
@@ -0,0 +1,32 @@
+package io.github.loncra.basic.service.ai.server.resolver.event;
+
+import io.agentscope.core.agent.RuntimeContext;
+import io.agentscope.core.event.AgentEvent;
+import io.agentscope.core.event.AgentStartEvent;
+import io.github.loncra.basic.service.ai.server.domain.entity.agent.AgentMessageEntity;
+import io.github.loncra.basic.service.ai.server.domain.metadata.AbstractAssistantMessageContentMetadata;
+import io.github.loncra.basic.service.ai.server.domain.metadata.content.AgentStatusContentMetadata;
+import io.github.loncra.basic.service.ai.server.enumerate.agent.AgentChatStatusEnum;
+import io.github.loncra.basic.service.ai.server.resolver.AgentEventResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AgentStartEventResolver implements AgentEventResolver {
+
+    @Override
+    public boolean isSupport(AgentEvent event) {
+        return AgentStartEvent.class.isAssignableFrom(event.getClass());
+    }
+
+    @Override
+    public AbstractAssistantMessageContentMetadata process(
+            AgentMessageEntity assistant,
+            AgentEvent event,
+            RuntimeContext context
+    ) {
+        AgentStatusContentMetadata result = new AgentStatusContentMetadata();
+        result.setId(assistant.getAgentConversationId().toString());
+        result.setStatus(AgentChatStatusEnum.RUNNING);
+        return result;
+    }
+}
